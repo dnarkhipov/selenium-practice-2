340aa273bada9117cd30022d68d1fbd80a562351
Dmitry N Arkhipov <arkhipov@alfasatcom.ru>
2021-02-23 11:43:21 +0300

    Шаг 6. Подготовка окружения

---
+++ conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome (default) or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language, default <en>")
+
+
+@pytest.fixture
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    driver = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        driver = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        driver = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    driver.implicitly_wait(15)
+    yield driver
+    print("\nquit browser..")
+    driver.quit()
---
+++ readme.md
@@ -0,0 +1,23 @@
+### [**Автоматизация тестирования с помощью Selenium и Python**][*]
+
+#### **Задачи раздела 4**
+
+#### ChromeDriver
+
+https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+    unzip chromedriver_mac64.zip
+    sudo mv chromedriver /usr/local/bin
+
+    chromedriver --version
+
+#### GeckoDriver
+
+https://github.com/mozilla/geckodriver/releases/
+
+    tar zxvf geckodriver-v0.29.0-macos.tar.gz
+    sudo mv geckodriver /usr/local/bin
+
+    geckodriver --version
+
+[*]: https://stepik.org/course/575
