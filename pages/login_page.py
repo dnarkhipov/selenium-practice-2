a901082ddc37682891fe72fd7e8103b547fa8e99
Dmitry N Arkhipov <arkhipov@alfasatcom.ru>
2021-02-23 16:11:53 +0300

    реализация LoginPage

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import MainPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.browser.current_url.find('login') >= 0, "URL of the current page is invalid"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.browser.find_element(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.browser.find_element(*MainPageLocators.REGISTER_FORM), "Register form is not presented"
