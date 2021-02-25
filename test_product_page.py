327dcbb6c6086eb056d19e3aea4851a011d91114
Dmitry N Arkhipov <arkhipov@alfasatcom.ru>
2021-02-25 21:44:06 +0300

    solve step 43_6

--- test_product_page.py
+++ test_product_page.py
@@ -25,9 +25,32 @@ product_links_3 = [
     "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
 ]
 
+
 @pytest.mark.parametrize('link', product_links_1)
-@pytest.mark.skip
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket_bug(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+
+    product_title = page.get_product_title()
+    product_price = page.get_product_price_text()
+
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+
+    # Сообщение о том, что товар добавлен в корзину. Название товара в сообщении должно совпадать с тем товаром,
+    # который вы действительно добавили.
+    basket_title_text = page.browser.find_element(*ProductPageLocators.BASKET_ALERT_TITLE).text
+    assert basket_title_text == product_title, \
+        'Название товара в сообщении не совпадает с тем товаром, который добавили'
+
+    # Сообщение со стоимостью корзины. Стоимость корзины совпадает с ценой товара.
+    basket_price_text = page.browser.find_element(*ProductPageLocators.BASKET_ALERT_PRICE).text
+    # Стоимость корзины теперь составляет <strong>9,99&nbsp;£</strong>
+    assert basket_price_text.find(product_price) >= 0, 'Стоимость корзины не совпадает с ценой товара'
+
+
+@pytest.mark.parametrize('link', product_links_2)
+def test_guest_can_add_product_to_basket_bug(browser, link):
     page = ProductPage(browser, link)
     page.open()
 
@@ -50,6 +73,7 @@ def test_guest_can_add_product_to_basket(browser, link):
 
 
 @pytest.mark.parametrize('link', product_links_3)
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
     page = ProductPage(browser, link, timeout=0)
     page.open()
@@ -69,6 +93,7 @@ def test_guest_cant_see_success_message(browser, link):
 
 
 @pytest.mark.parametrize('link', product_links_3)
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page = ProductPage(browser, link, timeout=0)
     page.open()
